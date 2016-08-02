b7b9422b9021c8ff340aec4fa8beb9b92a8ad206
Leo <Leo@POLO-SDD0>
2016-08-02 07:36:06 +0100

    Apple with failing attribute tests

--- src/Apple.java
+++ src/Apple.java
@@ -1,4 +1,48 @@
-
+/**
+ * Class to represent an apple
+ * @author Leo
+ *
+ */
 public class Apple {
+	private String colour;
+	private double weight;
+	private int taste;  // Taste 1, 2, 3, 4
+	private boolean wormInside;
+	
+	public Apple(String colour, double weight, int taste, boolean wormInside)
+	{
+		
+	}
+	
+	/**
+	 * Returns the colour of the apple
+	 * @return
+	 */
+	public String getColour() {
+		return colour;
+	}
 
+	/**
+	 * Returns the weight of the apple
+	 * @return
+	 */
+	public double getWeight() {
+		return weight;
+	}
+
+	/**
+	 * Returns the taste setting of the apple
+	 * @return
+	 */
+	public int getTaste() {
+		return taste;
+	}
+
+	/**
+	 * Returns the weight of the apple
+	 * @return
+	 */
+	public boolean hasWormInside() {
+		return wormInside;
+	}
 }
--- src/AppleTest.java
+++ src/AppleTest.java
@@ -2,17 +2,62 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
-
+/**
+ * Class to test the representation of an apple
+ * @author Leo
+ *
+ */
 public class AppleTest {
 
+	private Apple apple;
+	
 	@Before
 	public void setUp() throws Exception {
+		apple = new Apple("", 10, 0, true);
 	}
-
+	
+	/**
+	 * Test apple can be instantiated
+	 */
 	@Test
 	public void testCreateApple() {
-		Apple apple = new Apple();
 		assertNotNull(apple);
 	}
+	
+	/**
+	 * Test apple colour
+	 */
+	@Test
+	public void testAppleHasColour() {
+		assertNotNull(apple.getColour());
+		
+	}
 
+	/**
+	 * Test apple weight
+	 */
+	@Test
+	public void testAppleHasWeight() {
+		assertNotEquals(0, apple.getWeight(), 0);
+		
+	}
+
+	/**
+	 * Test apple taste
+	 */
+	@Test
+	public void testAppleHasTaste() {
+		assertNotEquals(0, apple.getTaste());
+		
+	}
+	
+	/**
+	 * Test apple worm flag
+	 */
+	@Test
+	public void testAppleHasWorm() {
+		assertTrue(apple.hasWormInside());
+		
+	}
+	
 }
